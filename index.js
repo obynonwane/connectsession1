385cd236c7ac3dd57a54f6957001434a8045f93f
obinna <biostechnologyng@gmail.com>
2022-12-03 14:43:25 +0100

    Update

--- index.js
+++ index.js
@@ -13,12 +13,16 @@ class newSession {
     },
   };
 
+  static myStaticMethod() {
+    console.log("Printing static");
+  }
   displayInfo() {
     for (let i = 0; i < this.listOfPlates.length; i++) {
       // defining  conditionals
       if (this.listOfPlates[i] == "yellow") {
         console.log(`Mommy I found ${this.listOfPlates[i]} plate`);
         console.log(this.person.takeItTomommy());
+        console.log(newSession.myStaticMethod());
         return;
       } else {
         console.log(`Mommy I found ${this.listOfPlates[i]} plate`);
@@ -31,28 +35,4 @@ class newSession {
 const newClass = new newSession();
 
 console.log(newClass.displayInfo());
-// //Defined an array
-
-// //JSON Object
-// const person = {
-//   first_name: "John",
-//   last_name: "Doe",
-//   complexion: "black",
-//   age: 23,
-//   height: "5.7ft",
-//   takeItTomommy: () => {
-//     console.log("Bring it to my room");
-//   },
-// };
-
-// //defined a loop
-// for (let i = 0; i < listOfPlates.length; i++) {
-//   //defining  conditionals
-//   if (listOfPlates[i] == "yellow") {
-//     console.log(`Mommy I found ${listOfPlates[i]} plate`);
-//     console.log(person.takeItTomommy());
-//     return;
-//   } else {
-//     console.log(`Mommy I found ${listOfPlates[i]} plate`);
-//   }
-// }
+console.log(newSession.myStaticMethod());
