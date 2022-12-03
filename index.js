6bdef697c79c0b152dcb04311ffd15ade0a5049e
obinna <biostechnologyng@gmail.com>
2022-12-03 14:35:29 +0100

    update

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
-//Defined an array
-const listOfPlates = ["white", "black", "yellow", "red"];
+class newSession {
+  listOfPlates = ["white", "black", "yellow", "red"];
+  constructor() {}
 
-//JSON Object
-const person = {
-  first_name: "John",
-  last_name: "Doe",
-  complexion: "black",
-  age: 23,
-  height: "5.7ft",
-  takeItTomommy: () => {
-    console.log("Bring it to my room");
-  },
-};
+  person = {
+    first_name: "John",
+    last_name: "Doe",
+    complexion: "black",
+    age: 23,
+    height: "5.7ft",
+    takeItTomommy: () => {
+      console.log("Bring it to my room");
+    },
+  };
 
-//defined a loop
-for (let i = 0; i < listOfPlates.length; i++) {
-  //defining  conditionals
-  if (listOfPlates[i] == "yellow") {
-    console.log(`Mommy I found ${listOfPlates[i]} plate`);
-    console.log(person.takeItTomommy());
-    return;
-  } else {
-    console.log(`Mommy I found ${listOfPlates[i]} plate`);
+  displayInfo() {
+    for (let i = 0; i < this.listOfPlates.length; i++) {
+      // defining  conditionals
+      if (this.listOfPlates[i] == "yellow") {
+        console.log(`Mommy I found ${this.listOfPlates[i]} plate`);
+        console.log(this.person.takeItTomommy());
+        return;
+      } else {
+        console.log(`Mommy I found ${this.listOfPlates[i]} plate`);
+      }
+    }
+    console.log("Welcome People");
   }
 }
+
+const newClass = new newSession();
+
+console.log(newClass.displayInfo());
+// //Defined an array
+
+// //JSON Object
+// const person = {
+//   first_name: "John",
+//   last_name: "Doe",
+//   complexion: "black",
+//   age: 23,
+//   height: "5.7ft",
+//   takeItTomommy: () => {
+//     console.log("Bring it to my room");
+//   },
+// };
+
+// //defined a loop
+// for (let i = 0; i < listOfPlates.length; i++) {
+//   //defining  conditionals
+//   if (listOfPlates[i] == "yellow") {
+//     console.log(`Mommy I found ${listOfPlates[i]} plate`);
+//     console.log(person.takeItTomommy());
+//     return;
+//   } else {
+//     console.log(`Mommy I found ${listOfPlates[i]} plate`);
+//   }
+// }
